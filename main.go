bcc987d4e4153228ec70385f9cff9e67ba50d67f
Nika <njones@users.noreply.github.com>
2021-09-29 15:53:27 -0700

    Create main.go
    This is a simple create

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	str := "Hello, world!"
+	fmt.Printf("%#v\n", Breakdown1(str))
+	fmt.Printf("%#v\n", Breakdown2(str))
+	fmt.Printf("%#v\n", Breakdown3(str))
+	fmt.Printf("%#v\n", Breakdown4(str))
+	fmt.Printf("%#v\n", Breakdown5(str))
+
+	fmt.Printf("%q\n", AcceptStruct1(Breakdown1(str)))
+	fmt.Printf("%q\n", AcceptStruct1(Breakdown4(str)))
+}
+
+func Breakdown1(str string) struct {
+	hello string
+	world string
+} {
+	strs := strings.Split(str, " ")
+	return struct {
+		hello, world string
+	}{
+		hello: strs[0],
+		world: strs[1],
+	}
+}
+
+type brkdn2 = struct{ hello, world string }
+
+func Breakdown2(str string) brkdn2 {
+	strs := strings.Split(str, " ")
+	return brkdn2{hello: strs[0], world: strs[1]}
+}
+
+type Brkdn3 = struct{ hello, world string }
+
+func Breakdown3(str string) Brkdn3 {
+	strs := strings.Split(str, " ")
+	return Brkdn3{hello: strs[0], world: strs[1]}
+}
+
+type brkdn4 struct{ hello, world string }
+
+func Breakdown4(str string) brkdn4 {
+	strs := strings.Split(str, " ")
+	return brkdn2{hello: strs[0], world: strs[1]}
+}
+
+func Breakdown5(str string) struct{ hello, world string } {
+	strs := strings.Split(str, " ")
+	return brkdn2{hello: strs[0], world: strs[1]}
+}
+
+func AcceptStruct1(str struct{ hello, world string }) string {
+	return fmt.Sprintf("%s %s", str.hello, str.world)
+}
+
+func AcceptStruct2(str brkdn2) string {
+	return fmt.Sprintf("%s %s", str.hello, str.world)
+}
+
+func AcceptStruct3(str brkdn4) string {
+	return fmt.Sprintf("%s %s", str.hello, str.world)
+}
